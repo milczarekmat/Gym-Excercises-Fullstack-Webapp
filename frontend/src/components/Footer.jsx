828d8dea96ebb448bddbe34f6e7e5d9bf05b3423
Mateusz Milczarek <milczary.mm@gmail.com>
2024-05-15 12:38:44 +0200

    Fixed footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="mt-4 flex min-h-[10vh] items-center bg-black text-center text-white">
+    <div className="fixed bottom-0 mt-4 flex min-h-[7vh] w-full items-center bg-black text-center text-white opacity-85">
       <div className="w-full">Mateusz Milczarek 2024 ©</div>
     </div>
   )
